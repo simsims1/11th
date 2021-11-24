e5fa89d78894ebc5d428aee5ecaad0a98f13ebd1
memor <memoryjuly@naver.com>
2021-11-24 15:05:04 +0900

    init_11th_app

---
+++ app/src/main/java/com/example/a11th/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.a11th;
+
+public class MovieList {
+    MovieList boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/a11th/MovieListResult.java
@@ -0,0 +1,10 @@
+package com.example.a11th;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
